eebdb9054d22896297cad5c4fbe3f29352f70017
Jorni Cornelese <jornicornelese33@gmail.com>
2026-02-10 23:17:12 +0100

    Add support for resolving `@mixin` property types in `Resources` collector.

--- src/Collectors/Resources.php
+++ src/Collectors/Resources.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Collection;
 use Laravel\Ranger\Components\Resource as ResourceComponent;
 use Laravel\Surveyor\Analyzer\Analyzer;
 use Laravel\Surveyor\Types\ArrayType;
+use Laravel\Surveyor\Types\MixedType;
+use ReflectionClass;
 use Spatie\StructureDiscoverer\Discover;
 
 class Resources extends Collector
@@ -61,10 +63,106 @@ class Resources extends Collector
         $component = new ResourceComponent($class);
         $component->setFilePath($result->filePath());
 
+        $mixinProperties = $this->resolveMixinProperties($class);
+
         foreach ($returnType->value as $key => $type) {
+            if ($type instanceof MixedType && isset($mixinProperties[$key])) {
+                $type = $mixinProperties[$key];
+            }
+
             $component->addField($key, $type);
         }
 
         return $component;
+    }
+
+    /**
+     * Resolve property types from a @mixin class docblock annotation.
+     *
+     * @param  class-string  $class
+     * @return array<string, \Laravel\Surveyor\Types\Contracts\Type>
+     */
+    protected function resolveMixinProperties(string $class): array
+    {
+        $reflection = new ReflectionClass($class);
+        $docComment = $reflection->getDocComment();
+
+        if ($docComment === false) {
+            return [];
+        }
+
+        if (! preg_match('/@mixin\s+\\\\?([\w\\\\]+)/', $docComment, $matches)) {
+            return [];
+        }
+
+        $mixinClass = $this->resolveClassName($matches[1], $reflection);
+
+        if ($mixinClass === null || ! class_exists($mixinClass)) {
+            return [];
+        }
+
+        $scope = $this->analyzer->analyzeClass($mixinClass)->analyzed();
+
+        if ($scope === null) {
+            return [];
+        }
+
+        $properties = [];
+
+        foreach ($scope->state()->properties()->variables() as $name => $states) {
+            $last = end($states);
+
+            if ($last && $last->type() !== null) {
+                $properties[$name] = $last->type();
+            }
+        }
+
+        return $properties;
+    }
+
+    /**
+     * Resolve a short class name to its fully qualified name using the file's use statements.
+     */
+    protected function resolveClassName(string $name, ReflectionClass $reflection): ?string
+    {
+        // Already fully qualified
+        if (class_exists($name)) {
+            return $name;
+        }
+
+        // Parse use statements from the source file
+        $source = file_get_contents($reflection->getFileName());
+        $tokens = token_get_all($source);
+
+        for ($i = 0; $i < count($tokens); $i++) {
+            if (! is_array($tokens[$i]) || $tokens[$i][0] !== T_USE) {
+                continue;
+            }
+
+            $use = '';
+
+            for ($j = $i + 1; $j < count($tokens); $j++) {
+                if (is_array($tokens[$j]) && in_array($tokens[$j][0], [T_NAME_QUALIFIED, T_NAME_FULLY_QUALIFIED, T_STRING])) {
+                    $use .= $tokens[$j][1];
+                } elseif ($tokens[$j] === ';') {
+                    break;
+                }
+            }
+
+            $shortName = class_basename($use);
+
+            if ($shortName === $name && class_exists($use)) {
+                return $use;
+            }
+        }
+
+        // Try same namespace as the resource class
+        $sameNamespace = $reflection->getNamespaceName().'\\'.$name;
+
+        if (class_exists($sameNamespace)) {
+            return $sameNamespace;
+        }
+
+        return null;
     }
 }
